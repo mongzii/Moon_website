f14c355f2dd53cf2b0ad748810d9bfba57ae37bc
moonjiyoung <umarg228@naver.com>
2023-09-15 17:29:26 +0900

    feat: home 사진 넣는중

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getHomeTv, IGetHOMETVResult } from "../api";
 import { makeImagePath } from "../utils";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -27,14 +29,7 @@ const Greeting = styled.h2`
   font-size: 70px;
   color: gray;
 `;
-// const Title = styled.h3`
-//   font-size: 40px;
-//   margin-bottom: 20px;
-// `;
-// const Overview = styled.p`
-//   font-size: 25px;
-//   width: 50%;
-// `;
+
 const Title1 = styled.h3`
   color: white;
   font-size: 25px;
@@ -47,17 +42,27 @@ const Slider1 = styled.div`
   position: relative;
   top: 150px;
 `;
-const Row1 = styled.div`
+const Row1 = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   gap: 5px;
   width: 100%;
   position: absolute;
 `;
-const Box1 = styled.div`
+const rowVariants = {
+  hidden: { x: window.outerWidth + 5 },
+  visible: { x: 0 },
+  exit: { x: -window.outerWidth - 5 },
+};
+const Box1 = styled(motion.div)<{ bgPhoto: string }>`
   background-color: white;
   height: 200px;
   cursor: pointer;
+  color: red;
+  font-size: 40px;
+  background-image: url(${props => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
 `;
 const Title2 = styled.h3`
   color: white;
@@ -83,6 +88,7 @@ const Box2 = styled.div`
   height: 200px;
   cursor: pointer;
 `;
+const offset = 6;
 
 function Home() {
   const { data, isLoading } = useQuery<IGetHOMETVResult>(
@@ -90,6 +96,18 @@ function Home() {
     getHomeTv
   );
   console.log(data);
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const toggleLeaving = () => setLeaving(prev => !prev);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.ceil(totalMovies / offset) - 1;
+      setIndex(prev => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
   return (
     <>
       <Wrapper>
@@ -97,22 +115,35 @@ function Home() {
           <Loader>Loading...</Loader>
         ) : (
           <>
-            <Main bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Main
+              onClick={increaseIndex}
+              bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+            >
               <Greeting>Welcome to the MoonFlix</Greeting>
-              {/* <Title>{data?.results[1].title || data?.results[1].name}</Title>
-              <Overview>{data?.results[1].overview}</Overview> */}
             </Main>
 
-            <Title1>Tv</Title1>
+            <Title1 onClick={increaseIndex}>Tv</Title1>
             <Slider1>
-              <Row1>
-                <Box1 />
-                <Box1 />
-                <Box1 />
-                <Box1 />
-                <Box1 />
-                <Box1 />
-              </Row1>
+              <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                <Row1
+                  variants={rowVariants}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                  key={index}
+                  transition={{ type: "tween", duration: 1 }}
+                >
+                  {data?.results
+                    .slice(1)
+                    .slice(offset * index, offset * index + offset)
+                    .map(tv => (
+                      <Box1
+                        bgPhoto={makeImagePath(tv.backdrop_path, "w400")}
+                        key={tv.id}
+                      />
+                    ))}
+                </Row1>
+              </AnimatePresence>
             </Slider1>
 
             <Title2>Movies</Title2>
